dfcb9f76c57f343af7b9d9f38cb902a6ed278d65
uğur kağan çakır <80484731+ukcx@users.noreply.github.com>
2022-03-02 11:43:31 +0300

    Delete cs306_database systems_project directory

--- cs306_database systems_project/admin_page/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = mysqli_connect('localhost', 'root', '', 'airtravel');
-
-if($db->maxdb_connect_errno = 0){
-	die('Unable to connect to database [' . $db->maxdb_connect_error . ']');
-}
-
-?>
